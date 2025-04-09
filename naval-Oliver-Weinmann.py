0e617194ceeab0a97c55fd852d06e8ba5cd43034
nachuslotus <166507478+nachuslotus@users.noreply.github.com>
2025-04-08 21:22:46 -0300

    Basico mejorado chona
    Termine el básico hecho

--- naval-Oliver-Weinmann.py
+++ naval-Oliver-Weinmann.py
@@ -24,30 +24,33 @@ while maxChances < 10:
     columna: int = int(input('Ingrese la columna: '))
     maxChances +=1
     if(fila ==  0 and columna == 1):
-        print('Bien')
+       
         aciertos +=1
     elif(fila == 0 and columna == 7):
-        print('Bien')
+       
         aciertos +=1
     elif(fila == 3 and columna == 4):
-        print('Bien')
+       
         aciertos +=1
     elif(fila == 5 and columna == 4):
-        print('Bien')
+     
         aciertos +=1
     elif(fila == 6 and columna == 8):
-        print('Bien')
+       
         aciertos +=1
     elif(fila == 7 and columna == 1):
-        print('Bien')
+       
         aciertos +=1
     elif(fila == 7 and columna == 6):
-        print('Bien')
+       
         aciertos +=1
     else:
-        print ('Error')
         errores +=1
 
 
-print (aciertos)
-print (errores)
+
+# Estas líneas van después del if-else
+        print(aciertos)
+        print(errores)
+
+
